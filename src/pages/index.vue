eb5080416df043b1b070007cd81692f3f333c5ee
Pontakorn Paesaeng <37771900+pontakornth@users.noreply.github.com>
2021-12-11 16:51:23 +0700

    refactor: Use new year component instead

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,25 +1,3 @@
-<script setup lang="ts">
-const now = useNow()
-const currentYear = computed(() => now.value.getFullYear())
-const isNewYearDay = computed(() => {
-  const currentMonth = now.value.getMonth() // 0 = Jan
-  const currentDay = now.value.getDate() // 1 = 1 just like normal people
-  return currentMonth === 0 && currentDay === 1
-})
-</script>
-
 <template>
-  <div>
-    <h1 class="text-5xl font-sans font-bold" mb="4" text="blue-gray-800 dark:blue-gray-50">
-      <template v-if="isNewYearDay">
-        Happy New Year {{ currentYear }}
-      </template>
-      <template v-else>
-        New Year {{ currentYear + 1 }} Countdown
-      </template>
-    </h1>
-    <div>
-      <span class="text-4xl">23:12:00</span>
-    </div>
-  </div>
+  <NewYear />
 </template>
