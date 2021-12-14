fdc045553c23c116d121832b906423d5d4f257e2
Jaruwat Pohong <jara7897@gmail.com>
2021-12-14 07:35:28 +0700

    feat: reindeer-san start run for Santa-kun ❤️

--- src/components/Reindeer.vue
+++ src/components/Reindeer.vue
@@ -3,26 +3,75 @@ import reindeer from '~/assets/reindeer.png'
 </script>
 
 <template>
-  <img :src="reindeer" class="absolute reindeer" alt="reindeer">
+  <div class="reindeer-move">
+    <img :src="reindeer" class="absolute reindeer" alt="reindeer">
+  </div>
 </template>
 
 <style scoped>
+
+.reindeer-move {
+  position: absolute;
+  top: 10%;
+  left: 0;
+  width: 200px;
+  animation: reindeer-walk 5s linear infinite;
+  z-index: -1;
+}
+
+@keyframes reindeer-walk {
+  0% {
+    left: 0;
+    transform: scaleX(1);
+  }
+
+  48% {
+    left: calc(100% - 250px);
+    transform: scaleX(1);
+  }
+
+  52% {
+    left: calc(100% - 250px);
+    transform: scaleX(-1);
+  }
+
+  96% {
+    left: 0;
+    transform: scaleX(-1);
+  }
+
+  100% {
+    left: 0;
+    transform: scaleX(1);
+  }
+}
+
 .reindeer {
-  top: 20%;
-  left: 50%;
   animation-name: reindeer-spin;
-  animation-duration: 1.5s;
+  animation-duration: 0.5s;
   animation-timing-function: linear;
   animation-iteration-count: infinite;
-  animation-direction: alternate;
 }
 
 @keyframes reindeer-spin {
   0% {
-    transform: translateY(-10px);
+    transform: translateY(0) rotate(0);
   }
+
+  25% {
+    transform: translateY(-50px) rotate(-20deg);
+  }
+
+  50% {
+    transform: translateY(-70px) rotate(0deg);
+  }
+
+  75% {
+    transform: translateY(-50px) rotate(20deg);
+  }
+
   100% {
-    transform: translateY(10px);
+    transform: translateY(0) rotate(0deg);
   }
 }
 </style>
